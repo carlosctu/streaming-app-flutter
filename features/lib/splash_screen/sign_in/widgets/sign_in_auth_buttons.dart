2b235014bd9f8f9c78c3b8bedfc37ea305c8e8b1
carlosctu <carlosctu94@gmail.com>
2023-04-12 12:08:58 -0300

    fix: fixing ios folder problems

--- features/lib/splash_screen/sign_in/widgets/sign_in_auth_buttons.dart
+++ features/lib/splash_screen/sign_in/widgets/sign_in_auth_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:components/design_components.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SignInAuthButtons extends StatelessWidget {
   const SignInAuthButtons({
@@ -10,14 +11,39 @@ class SignInAuthButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final FirebaseAuth _auth = FirebaseAuth.instance;
-    
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    Future signInWithGoogle() async {
+      try {
+      // Realiza o login com o Google
+        final googleSignInAccount = await _googleSignIn.signIn();
+        final googleSignInAuthentication =
+            await googleSignInAccount!.authentication;
+
+        // Obtém as credenciais do Google
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
+
+        // Autentica com o Firebase usando as credenciais do Google
+        final UserCredential authResult =
+            await FirebaseAuth.instance.signInWithCredential(credential);
+
+        // Retorna as informações do usuário autenticado
+        return authResult;
+      } on FirebaseAuthException catch (e) {
+        print(e.message);
+      }
+    }
+
     return Wrap(
       alignment: WrapAlignment.center,
       runSpacing: 4,
       children: [
         StyledAuthButton(
           label: "Continue with Google",
-          onPressed: () {},
+          onPressed: signInWithGoogle,
           asset: StreamingAppAssets.google,
         ),
         StyledAuthButton(
