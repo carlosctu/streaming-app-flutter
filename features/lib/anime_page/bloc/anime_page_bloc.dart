5e23c50f97780da68c479b729225ff4630780782
carlosctu <carlosctu94@gmail.com>
2023-05-01 22:29:20 -0300

    feat: adding anime page controller

--- features/lib/anime_page/bloc/anime_page_bloc.dart
+++ features/lib/anime_page/bloc/anime_page_bloc.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:features/anime_page/mapper/anime_page_mapper.dart';
+import 'package:features/anime_page/model/get_anime_episode_info_view_data.dart';
 import 'package:features/anime_page/model/get_anime_episodes_info_view_data.dart';
 import 'package:features/anime_page/repository/anime_page_repository.dart';
+import 'package:features/anime_page/repository/model/get_anime_episode_info_response.dart';
 import 'package:features/home/bloc/home_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -23,12 +25,75 @@ class AnimePageBloc extends Bloc<AnimePageEvent, AnimePageState> {
   }
 
   void fetch() async {
+    List<String> episodesList = [];
     try {
       GetAnimeEpisodesIdViewData result = await _repository
           .getAnimeEpisodesId(animeId)
           .then((value) => value.toViewData());
-      print(result.data);
-      _dataController.sink.add(result);
+
+      episodesList = result.data.map((info) => info.id).toList();
+
+      List<Future<GetAnimeEpisodeInfoResponse>> futures = episodesList
+          .map((id) => _repository.getAnimeEpisodeInfo(id))
+          .toList();
+
+      List<GetAnimeEpisodeInfoResponse> episodeInfoList =
+          await Future.wait(futures);
+
+      final response = episodeInfoList.map((e) => e.toViewData());
+
+      print(response);
+
+      // final viewDataList = episodeInfoList.map((info) {
+      //   return info.toViewData();
+      // }).toList();
+
+      print(episodeInfoList);
+
+      final data = episodeInfoList.map((e) => e.toViewData()).toList();
+
+      // final response = await Future.wait(episodesList
+      //     .map(
+      //       (id) async => await _repository
+      //           .getAnimeEpisodeInfo(id)
+      //           .then((value) => value.toViewData()),
+      //     )
+      //     .toList());
+
+      // print(response);
+
+      // final episodesInfo = await fetchAnimeEpisodesInfo(episodesList);
+
+      // print(episodesInfo);
+      // _dataController.sink.add(episodesInfo);
+    } catch (ex) {
+      throw Exception(ex);
+    }
+  }
+
+  Future<List<GetAnimeEpisodeInfoViewData>> fetchAnimeEpisodesInfo(
+      List<String> episodesList) async {
+    List<GetAnimeEpisodeInfoViewData> response = [];
+    try {
+      final response = await _repository
+          .getAnimeEpisodeInfo(episodesList[0])
+          .then((value) => value.toViewData());
+      print(response);
+      final data = await Future.wait(episodesList
+          .map((id) async => _repository
+              .getAnimeEpisodeInfo(id)
+              .then((value) => value.toViewData()))
+          .toList());
+      print(data[0].data.attributes);
+      // print(response);
+      // final data = episodesList.map((id) async {
+      // final episodeInfo = await _repository
+      // .getAnimeEpisodeInfo(id)
+      // .then((value) => value.toViewData());
+      // response.add(episodeInfo);
+      // }).toList();
+      // print(data);
+      return Future.value([]);
     } catch (ex) {
       throw Exception(ex);
     }
