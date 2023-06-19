a5edc6c7b37fea2ddb01b8591d5112a556da9760
carlosctu <carlosctu94@gmail.com>
2023-06-18 21:05:28 -0300

    feat: adding FloatingBottomAppBar

--- features/lib/home/home_page.dart
+++ features/lib/home/home_page.dart
@@ -27,6 +27,7 @@ class _HomePageState extends State<HomePage> {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
+        // bottomNavigationBar: const MyWidget(),
         body: BlocBuilder<HomeBloc, HomeState>(
           builder: (context, state) {
             return StreamBuilder(
@@ -49,6 +50,47 @@ class _HomePageState extends State<HomePage> {
             );
           },
         ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      margin: EdgeInsets.all(12),
+      child: BottomNavigationBar(
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+              icon: IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  // _onTabTapped(0);
+                },
+              ),
+              label: '1'),
+          BottomNavigationBarItem(
+              icon: IconButton(
+                icon: Icon(Icons.access_alarm_rounded),
+                onPressed: () {
+                  // _onTabTapped(0);
+                },
+              ),
+              label: '2'),
+          BottomNavigationBarItem(
+              icon: IconButton(
+                icon: Icon(Icons.abc),
+                onPressed: () {
+                  // _onTabTapped(0);
+                },
+              ),
+              label: '3'),
+        ],
       ),
     );
   }
--- features/lib/home/widgets/home_body.dart
+++ features/lib/home/widgets/home_body.dart
@@ -1,10 +1,12 @@
+import 'dart:ui';
+
 import 'package:features/home/bloc/home_bloc.dart';
 import 'package:features/home/model/home_view_data.dart';
-import 'package:features/home/widgets/home_cover_widget.dart';
 import 'package:features/home/widgets/carousel_section/carousel_section.dart';
+import 'package:features/home/widgets/home_cover_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomeBody extends StatelessWidget {
+class HomeBody extends StatefulWidget {
   final Map<AnimeTypes, HomeViewData> data;
   const HomeBody({
     super.key,
@@ -12,50 +14,171 @@ class HomeBody extends StatelessWidget {
   });
 
   @override
+  State<HomeBody> createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 1,
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final maxHeight = MediaQuery.of(context).size.height;
     return SafeArea(
       top: false,
-      child: CustomScrollView(
-        physics: const BouncingScrollPhysics(),
-        slivers: [
-          SliverAppBar(
-            pinned: false,
-            floating: false,
-            automaticallyImplyLeading: false,
-            expandedHeight: maxHeight < 680 ? 350 : 420,
-            collapsedHeight: kToolbarHeight + 80,
-            backgroundColor: Colors.transparent,
-            flexibleSpace: HomeCoverWidget(
-              data: data[AnimeTypes.trending]!,
+      child: Stack(
+        children: [
+          PageView(
+            physics: const NeverScrollableScrollPhysics(),
+            controller: _pageController,
+            children: [
+              const Center(
+                child: Text('Search Page'),
+              ),
+              AnimeHomePage(data: widget.data),
+              const Center(
+                child: Text('User Page'),
+              ),
+            ],
+          ),
+          FloatingBottomAppBar(
+            controller: _pageController,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AnimeHomePage extends StatelessWidget {
+  final Map<AnimeTypes, HomeViewData> data;
+
+  const AnimeHomePage({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    final maxHeight = MediaQuery.of(context).size.height;
+
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverAppBar(
+          pinned: false,
+          floating: false,
+          automaticallyImplyLeading: false,
+          expandedHeight: maxHeight < 680 ? 350 : 420,
+          collapsedHeight: kToolbarHeight + 80,
+          backgroundColor: Colors.transparent,
+          flexibleSpace: HomeCoverWidget(
+            data: data[AnimeTypes.trending]!,
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.only(top: 12),
+          sliver: SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                CarouselSection(
+                  animes: data[AnimeTypes.trending]!.data,
+                  sectionTitle: 'Trending series',
+                ),
+                CarouselSection(
+                  animes: data[AnimeTypes.shounen]!.data,
+                  sectionTitle: 'Recently updated',
+                ),
+                CarouselSection(
+                  animes: data[AnimeTypes.romance]!.data,
+                  sectionTitle: 'Romance checkpoint',
+                ),
+                CarouselSection(
+                  animes: data[AnimeTypes.newUpdates]!.data,
+                  sectionTitle: 'Shounen series',
+                ),
+              ],
             ),
           ),
-          SliverPadding(
-            padding: const EdgeInsets.only(top: 12),
-            sliver: SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  CarouselSection(
-                    animes: data[AnimeTypes.trending]!.data,
-                    sectionTitle: 'Trending series',
-                  ),
-                  CarouselSection(
-                    animes: data[AnimeTypes.shounen]!.data,
-                    sectionTitle: 'Recently updated',
-                  ),
-                  CarouselSection(
-                    animes: data[AnimeTypes.romance]!.data,
-                    sectionTitle: 'Romance checkpoint',
-                  ),
-                  CarouselSection(
-                    animes: data[AnimeTypes.newUpdates]!.data,
-                    sectionTitle: 'Shounen series',
-                  ),
-                ],
+        ),
+      ],
+    );
+  }
+}
+
+class FloatingBottomAppBar extends StatelessWidget {
+  final PageController controller;
+  const FloatingBottomAppBar({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 16),
+        child: Align(
+          alignment: AlignmentDirectional.bottomCenter,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 6, sigmaY: 3),
+              child: LayoutBuilder(
+                builder: (context, constrains) {
+                  return Container(
+                    padding: EdgeInsets.zero,
+                    color: Colors.black.withOpacity(0.3),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          color: Colors.white,
+                          icon: Container(
+                            height: constrains.maxHeight,
+                            width: constrains.maxWidth,
+                            color: const Color(0xffD93B41),
+                            child: const Icon(Icons.search),
+                          ),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(width: 12),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          color: Colors.white,
+                          icon: Container(
+                            height: constrains.maxHeight,
+                            width: constrains.maxWidth,
+                            color: const Color(0xffD93B41),
+                            child: const Icon(Icons.home),
+                          ),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(width: 12),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          color: Colors.white,
+                          icon: Container(
+                            height: constrains.maxHeight,
+                            width: constrains.maxWidth,
+                            color: const Color(0xffD93B41),
+                            child: Icon(Icons.person),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
